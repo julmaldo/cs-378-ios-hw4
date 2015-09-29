dd2f08ca06c7c736a4ad08ffdb7f63c1876979ba
julmaldo <julmaldo>
2015-09-29 14:51:15 -0500

    init commit

---
+++ MaldonadoJulio-hw4Tests/MaldonadoJulio_hw4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MaldonadoJulio_hw4Tests.swift
+//  MaldonadoJulio-hw4Tests
+//
+//  Created by Julio Maldonado on 9/29/15.
+//  Copyright © 2015 Julio Maldonado. All rights reserved.
+//
+
+import XCTest
+@testable import MaldonadoJulio_hw4
+
+class MaldonadoJulio_hw4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
